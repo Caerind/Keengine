f36c5c02c200781dbf514e179d34a09a9062cb7d
Cmdu76 <charles.mailly@free.fr>
2016-08-22 20:07:38 +0200

    Add functions to get the OS in code

--- Keengine/Sources/Config.hpp
+++ Keengine/Sources/Config.hpp
@@ -5,32 +5,119 @@
 
 #ifndef SFML_STATIC
 	#ifdef SFML_SYSTEM_WINDOWS
-		// Windows compilers need specific (and different) keywords for export and import
-		#ifdef KEENGINE_EXPORTS
+		#ifdef KEENGINE_EXPORTS // Windows compilers need specific (and different) keywords for export and import
 			#define KEENGINE_API __declspec(dllexport)
 		#else
 			#define KEENGINE_API __declspec(dllimport)
 		#endif
 	#else // Linux, FreeBSD, Mac OS X
-		// GCC 4 has special keywords for showing/hidding symbols
-		#if __GNUC__ >= 4
+		#if __GNUC__ >= 4 // GCC 4 has special keywords for showing/hidding symbols
 			#define KEENGINE_API __attribute__((__visibility__("default")))
 		#else
 			#define KEENGINE_API
 		#endif
 	#endif
 #else
-	// Static build doesn't need import/export macros
 	#define KEENGINE_API
 #endif
 
-#ifdef SFML_SYSTEM_ANDROID
-#define KEENGINE_ANDROID
+#if SFML_SYSTEM_ANDROID || SFML_SYSTEM_IOS
+	#define KEENGINE_MOBILE
+	#ifdef SFML_SYSTEM_ANDROID
+		#define KEENGINE_ANDROID
+	#else
+		#define KEENGINE_IOS
+	#endif
 #else
-#define KEENGINE_DESKTOP
+	#define KEENGINE_DESKTOP
+	#ifdef SFML_SYSTEM_WINDOWS
+		#define KEENGINE_WINDOWS
+	#elif SFML_SYSTEM_MACOS
+		#define KEENGINE_MACOS
+	#elif SFML_SYSTEM_LINUX
+		#define KEENGINE_LINUX
+	#elif SFML_SYSTEM_FREEBSD
+		#define KEENGINE_FREEBSD
+	#endif
 #endif
 
 #define KEENGINE_VERSION_MAJOR 0
 #define KEENGINE_VERSION_MINOR 1
+
+namespace ke
+{
+	inline bool isMobile()
+	{
+		#ifdef KEENGINE_MOBILE
+		return true;
+		#else
+		return false;
+		#endif
+	}
+
+	inline bool isAndroid()
+	{
+		#ifdef KEENGINE_ANDROID
+		return true;
+		#else
+		return false;
+		#endif		
+	}
+
+	inline bool isIOS()
+	{
+		#ifdef KEENGINE_IOS
+		return true;
+		#else
+		return false;
+		#endif		
+	}
+
+	inline bool isDesktop()
+	{
+		#ifdef KEENGINE_DESKTOP
+		return true;
+		#else
+		return false;
+		#endif
+	}
+
+	inline bool isWindows()
+	{
+		#ifdef KEENGINE_WINDOWS
+		return true;
+		#else
+		return false;
+		#endif	
+	}
+
+	inline bool isMacOS()
+	{
+		#ifdef KEENGINE_MACOS
+		return true;
+		#else
+		return false;
+		#endif	
+	}
+
+	inline bool isLinux()
+	{
+		#ifdef KEENGINE_LINUX
+		return true;
+		#else
+		return false;
+		#endif	
+	}
+
+	inline bool isFreeBSD()
+	{
+		#ifdef KEENGINE_FREEBSD
+		return true;
+		#else
+		return false;
+		#endif	
+	}
+
+}
 
 #endif // KE_CONFIG_HPP
