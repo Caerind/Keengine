68cda6dd233408fbd3d699a8da2c8f23e656c47e
Charles Mailly <charles.mailly@free.fr>
2016-10-24 14:49:45 +0200

    Variant.hpp

--- Keengine/Sources/System/Variant.hpp
+++ Keengine/Sources/System/Variant.hpp
@@ -10,50 +10,23 @@ class Variant : public std::string
 {
 	public:
 		template <typename T>
-		Variant(const T &t) : std::string(std::to_string(t))
+		Variant(const T& value) : std::string(toString<T>(value))
 		{}
 
-		Variant(bool boolean) : std::string(toString<bool>(boolean))
-		{}
-
-		template <size_t N>
-		Variant(const char(&s)[N]) : std::string(s, N)
-		{}
-
-		Variant(const char *cstr) : std::string(cstr)
-		{}
-
-		Variant(const std::string &other = std::string()) : std::string(other)
-		{}
-
-		template <typename T>
-		Variant(sf::Vector2<T> const& vector) : std::string(toString(vector))
-		{}
-
-		Variant(sf::Color const& color) : std::string(toString(color))
-		{}
-
-		template <typename T> operator T() const
+		template <typename T> bool operator==(const T& value) const
 		{
-			T t;
-			std::stringstream ss;
-			return ss << *this && ss >> t ? t : T();
+		    return this->compare(Variant(value)) == 0;
 		}
 
-		template <typename T> bool operator==(const T &t) const
+		bool operator==(const char* value) const
 		{
-			return 0 == this->compare(Variant(t));
-		}
-
-		bool operator==(const char *t) const
-		{
-			return this->compare(t) == 0;
+		    return this->compare(value) == 0;
 		}
 
 		template <typename T>
 		T as()
 		{
-			return fromString<T>(*this);
+		    return fromString<T>(*this);
 		}
 };
 
